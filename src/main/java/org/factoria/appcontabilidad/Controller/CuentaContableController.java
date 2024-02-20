ef295dff57b286c28b40b444691d9b5983ea089b
Marta <martalarena2002@gmail.com>
2024-02-20 12:52:18 +0100

    tests  de integrACION II

--- src/main/java/org/factoria/appcontabilidad/Controller/CuentaContableController.java
+++ src/main/java/org/factoria/appcontabilidad/Controller/CuentaContableController.java
@@ -1,5 +1,24 @@
 package org.factoria.appcontabilidad.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/asientos")
 public class CuentaContableController {
-    
+
+    @PostMapping
+    // "/asientos?code=14&amount=1200"
+    public String nuevoAsiento(
+            @RequestParam(name = "code", required = true) Integer code,
+            @RequestParam(name = "amount", required = true) Integer amount) {
+
+        System.out.println("Me han pasado " + code + " y " + amount);
+
+        return "Registrado asiento contable. Importe: 400 EURO. Tipo de cuenta: Ingresos. Código de cuenta: 701. Nombre de cuenta: Ventas de productos terminados";
+    }
 }
--- src/test/java/org/factoria/appcontabilidad/IntegrationTests.java
+++ src/test/java/org/factoria/appcontabilidad/IntegrationTests.java
@@ -3,6 +3,8 @@ package org.factoria.appcontabilidad;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,19 +14,38 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 @SpringBootTest
 @AutoConfigureMockMvc
 public class IntegrationTests {
 
     @Autowired
     MockMvc api;
-    
+
+    @Test
+    public void registrar_ventas_por_400_euros() throws Exception {
+
+        api.perform(post("/asientos?code=701&amount=400"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        "Registrado asiento contable. Importe: 400 EURO. Tipo de cuenta: Ingresos. Código de cuenta: 701. Nombre de cuenta: Ventas de productos terminados"));
+
+    }
+
+    @Test
+    public void registrar_provision_por_riesgos_1200_euros() throws Exception {
+
+        api.perform(post("/asientos?code=14&amount=1200"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        "Registrado asiento contable. Importe: 1200 EURO. Tipo de cuenta: Gastos. Código de cuenta: 14. Nombre de cuenta: PROVISIONES PARA RIESGOS Y GASTOS"));
+
+    }
+
     // @Test
     // public void test_de_ejemplo() throws Exception {
 
-    //     api.perform(get("/books/14"))
-    //         .andExpect(status().isOk())
-    //         .andExpect(jsonPath("$title", equalTo("El señor del los anillos")));
+    // api.perform(get("/books/14"))
+    // .andExpect(status().isOk())
+    // .andExpect(jsonPath("$title", equalTo("El señor del los anillos")));
     // }
 }
